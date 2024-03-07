f1d0e107dc468aeb29ef2743a3b9a3737ec7bf07
mayaraskin <mayaraskin14@gmail.com>
2024-03-07 12:32:57 +0200

    Added useGeographic() call

--- src/components/map-page/mapwrapper/MapWrapper.tsx
+++ src/components/map-page/mapwrapper/MapWrapper.tsx
@@ -3,11 +3,13 @@ import Map from 'ol/Map'
 import {createRef, FC, useEffect, useRef} from "react";
 import Features from "./features/Features.tsx";
 import {useMap} from "../../../hooks/context/map/common.ts";
+import {useGeographic} from "ol/proj";
 
 const MapWrapper: FC = () => {
     const map = useMap();
     const mapContainerRef = createRef();
     const mapRef = useRef<Map | null>(map);
+    useGeographic()
 
     useEffect(() => {
         if (!map) return;
